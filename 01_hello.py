c84336a913eebd5136b3420a9d1cb37f996331ad
Dhruv Rai <70002443+Dhruv-Rai@users.noreply.github.com>
2023-09-01 18:41:45 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,7 @@
+print('hello world')
+print(5)
+print("Hey i am a \"good boy\" \n and this viewer is also a good boy")
+# hey harry please dont remove this line
+print("hey",6,7,sep="~",end="009\n")
+a=1
+print (a)
