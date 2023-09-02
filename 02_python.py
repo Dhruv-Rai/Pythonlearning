cc3bb66cc3291fe19888d069bf2d61de4bea4a81
Dhruv Rai <70002443+Dhruv-Rai@users.noreply.github.com>
2023-09-02 10:22:06 +0530

    Add files via upload

---
+++ 02_python.py
@@ -0,0 +1,16 @@
+a=complex(8,2)
+b="harry"
+c=True
+d=None
+print(a)
+print(b)
+print("the type of a is ",type(a))
+print("the type of b is ",type(b))
+print("the type of c is ",type(c))
+print("the type of d is ",type(d))
+list1=[8,2.3,[4,-5],"Dhruv",["apple","mango"]]
+print(list1)
+tuple1=(("parrot","sparrow"),("lion","tiger"))
+print(tuple1)
+dict1={"name":"sakshi","age":20,"canVote":True}
+print (dict1)
