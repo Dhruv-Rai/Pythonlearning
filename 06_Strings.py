3aa7f9f6ebc0c5759a33ecea55dc018e9582d8da
Dhruv Rai <70002443+Dhruv-Rai@users.noreply.github.com>
2023-09-03 10:01:04 +0530

    Add files via upload

---
+++ 06_Strings.py
@@ -0,0 +1,16 @@
+name='Dhruv'
+apple= '''he said 
+i am good 
+he wants to have an apple'''
+print ("hello "+ name )
+print (apple)
+print(name[0])
+# in python string is like an array of character
+# index error when the index is not available in the string
+# we can loop through the string using for loop
+for character in name:
+    print(character)
+    names = "Dhruv,harry"
+    print(name[0:5])
+    print(len(names))
+
