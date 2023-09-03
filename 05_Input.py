f5ae2465bbd586b6d38421bed203be5db545aae2
Dhruv Rai <70002443+Dhruv-Rai@users.noreply.github.com>
2023-09-03 10:00:07 +0530

    Add files via upload

---
+++ 05_Input.py
@@ -0,0 +1,7 @@
+# we can take user input directly using input function and store it in a variable
+a=input("Enter you name:")
+print("My name is:",a)
+x=input("Enter the first Number:")
+y=input("Enter the second Number:")
+print(x+y)
+print(int(x)+int(y))
