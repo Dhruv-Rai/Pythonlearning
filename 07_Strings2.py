a4000d4611ade0a3787250830a0c00f462c6afd1
Dhruv Rai <70002443+Dhruv-Rai@users.noreply.github.com>
2023-09-04 14:15:21 +0530

    Add files via upload

---
+++ 07_Strings2.py
@@ -0,0 +1,11 @@
+name="Dhruv,Harry"
+print(name[0:5])
+print(len(name))
+fruit="mango"
+print("mango is a",len(fruit),"character word")
+print(fruit[0:4])
+print(fruit[:-3])
+print(fruit[1:5]) # including the first but not including the last
+nm="harry"
+print(nm[-4:-2])
+
