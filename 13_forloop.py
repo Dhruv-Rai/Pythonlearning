6965f6f1cac46ccd51ce8c3b03058be080f983d5
Dhruv Rai <70002443+Dhruv-Rai@users.noreply.github.com>
2023-09-06 10:24:26 +0530

    Add files via upload

---
+++ 13_forloop.py
@@ -0,0 +1,16 @@
+name = 'Dhruv'
+for i in name:
+    print(i)
+    if(i=="i"):
+        print("This is something special")
+colors= ['Red','Green','Blue','Yellow']
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+for k in range(5):
+    print(k+1)
+# for k in range(1,20001):
+#    print(k)
+for k in range(1,100,3): # step leaves the number of counting mentioned at end
+    print(k)
